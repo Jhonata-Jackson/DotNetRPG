cd535e2e9443ea891b8f6aa1c4c8397ce193e01b
Jhonata-Jackson <jhonata.jacksondev@gmail.com>
2023-07-27 23:14:07 -0300

    feat: create Character Model and Controller

---
+++ DotNetRPG/Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using DotNetRPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetRPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static Character Knight = new Character();
+        
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(Knight);
+        }
+    }
+}
+
